02e796f9c58d89ee77e9240dccd909c9622d85af
nibir-code <mubtashimfuadnibir@gmail.com>
2022-05-19 15:50:26 +0600

    component tree

--- src/components/mainComponent.js
+++ src/components/mainComponent.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Body from "./Body/Body";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
 
 const mainComponent = () => {
      return (
           <div>
-               
+               <Header/>
+               <Body/>
+               <Footer/>
           </div>
      );
 };
